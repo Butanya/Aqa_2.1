3becb5ddd319ce27049391070c76aa9631538ceb
Tanya <tosi4bosi@gmail.com>
2022-07-28 22:36:11 +0300

    init commit, fail

---
+++ src/test/java/ru/netology/postman/PostmanEchoTest.java
@@ -0,0 +1,23 @@
+package ru.netology.postman;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldPostData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Lorem ipsum dolor sit amet")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Lore ipsum dolor sit amet"));
+    }
+}
